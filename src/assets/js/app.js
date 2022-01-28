83fd51bf64510c7f5a371d30feaa320c5bbe745d
Beezwax99 <shkarbulo@gmail.com>
2022-01-28 17:30:53 +0200

    commit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,33 @@
 
-var myCarousel = document.querySelector('#carousel')
-var carousel = new bootstrap.Carousel(myCarousel)
+//header menu
+const menu = $('.mob'),
+burger = $('.burger');
+burger.on('click', function (e) {
+e.preventDefault();
+if ( $(this).hasClass('active') ) {
+    menu.removeClass('active')
+    $('body').removeClass('no-scroll');
+    $('html').removeClass('no-scroll');
+    setTimeout(() => {
+        $('.header').removeClass('active');
+        $('.header').removeClass('header--scroll');
+    }, 400);
+    }  else {
+        menu.addClass('active')
+        $('body').addClass('no-scroll');
+        $('html').addClass('no-scroll');
+        $('.header').addClass('active');
+        $('.header').removeClass('header--scroll');
+    }
+}); 
+$('.close').on('click', function (e) {
+    e.preventDefault();
+    menu.removeClass('active')
+    
+    $('body').removeClass('no-scroll');
+    $('html').removeClass('no-scroll');
+    setTimeout(() => {
+        $('.header').removeClass('active');
+        $('.header').removeClass('header--scroll');
+    }, 400);
+});
